5d1ac7de0e62df2b874af4e7ddf255bfb6f7e42e
Joshua Kwan <joshua.kwan@liveramp.com>
2017-10-20 10:53:57 -0400

    Use a 'codestamp' instead of a 'timestamp'
    Prevents multiple runs of render_deploys and docker_deploy_tag from
    racing each other.

--- lib/cluster_config.rb
+++ lib/cluster_config.rb
@@ -1,14 +1,25 @@
+require 'digest'
 require 'etc'
 require 'time'
 
 # Default method to derive a tag name based on the current environment.
 def tag_from_local_env
-  timestamp = DateTime.now.strftime('%j.%H.%M.%S')
+  codestamp = `git rev-parse --short=7 HEAD`.rstrip
+  # If tree is dirty, take a hash sum of the output of git status -s as well as
+  # git diff to try to encapsulate the state of the environment. Uniqueness is
+  # all that matters here.
+  status = `git status -s`
+  if !status.empty?
+    diff = `git diff`
+    dirty_sum = Digest::MD5.hexdigest(status + diff)
+    codestamp += "-dirty#{dirty_sum}"
+  end
+
   branch = ENV['GIT_BRANCH'] || 'LOCAL'
   if branch.start_with?('origin/')
     branch = branch['origin/'.size..-1]
   end
-  "#{branch}-#{ENV['GIT_COMMIT'] || timestamp}"
+  "#{branch}-#{ENV['GIT_COMMIT'] || codestamp}"
 end
 
 DEFAULT_REGISTRY = '***REMOVED***'
