2d569fbd25a99f54f4a6e585c0a2e01e8a109b8a
Peter Hu <peter.hu@liveramp.com>
2017-11-01 19:15:11 -0700

    implements streaming output from shellrunner commands

--- lib/kube_deploy_tools/shellrunner.rb
+++ lib/kube_deploy_tools/shellrunner.rb
@@ -16,8 +16,33 @@ module KubeDeployTools
 
     def run_call(*cmd)
       @logger.info(Shellwords.join(cmd))
-      out, err, status = Open3.capture3(*cmd)
-      @logger.debug(out.shellescape)
+      # Save the entire stdout and stderr output of the subprocess
+      # to return at the end
+      out = ''
+      err = ''
+
+      # Stream stdout and stderr output of the subprocess
+      # Makes logs appear in realtime for long running processes
+      status = Open3.popen3(*cmd) do |stdin, stdout, stderr, thread|
+        # read each stream from a new thread
+        { :out => stdout, :err => stderr }.each do |key, stream|
+          Thread.new do
+            until (line = stream.gets).nil? do
+              if key == :out
+                @logger.debug line.strip
+                out << line
+              else
+                @logger.warn line.strip
+                err << line
+              end
+            end
+          end
+        end
+
+        thread.join
+        thread.value
+      end
+
 
       if !status.success?
         @logger.warn("The following command failed: #{Shellwords.join(cmd)}")
