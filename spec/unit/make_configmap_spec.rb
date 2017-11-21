ad92b39afb1e43520baa5cce187484c62c6976d9
Julian V. Modesto <julian.modesto@liveramp.com>
2017-11-21 15:23:15 -0500

    Add unit tests

--- spec/unit/make_configmap_spec.rb
+++ spec/unit/make_configmap_spec.rb
@@ -2,17 +2,76 @@ require 'tmpdir'
 require 'kube_deploy_tools/make_configmap'
 require 'kube_deploy_tools/make_configmap/options'
 
-CONFIGMAP_FILENAME_TEMPLATE="haproxy.cfg.erb"
-CONFIGMAP_CONTENT_FILE="haproxy.yaml"
-CONFIGMAP_CONTENT="spec/resources/kubernetes/config/#{CONFIGMAP_CONTENT_FILE}"
-CONFIGMAP_FILEPATH="spec/resources/kubernetes/config/#{CONFIGMAP_FILENAME_TEMPLATE}"
+
+CONFIGMAP_DATA_FILE_NAME="haproxy.cfg"
+CONFIGMAP_DATA_DIRECTORY_PATH="spec/resources/config/"
+CONFIGMAP_DATA_FILE_PATH=File.join(CONFIGMAP_DATA_DIRECTORY_PATH, CONFIGMAP_DATA_FILE_NAME)
+CONFIGMAP_DATA_FILE_CONTENT=File.read(CONFIGMAP_DATA_FILE_PATH)
+
+CONFIGMAP_NAME="my-haproxy-config"
+CONFIGMAP_DEFAULT_NAMESPACE="default"
+CONFIGMAP_FILE_PATH="spec/resources/kubernetes/configmap/configmap-haproxy.yaml"
+CONFIGMAP_FILE_CONTENT=File.read(CONFIGMAP_FILE_PATH)
 
 describe KubeDeployTools::ConfigMap do
-  it "writes to output a string " do
-    Dir.mktmpdir do |tmp_dir|
-      make_configmap = KubeDeployTools::ConfigMap.new('test', [CONFIGMAP_FILEPATH], 'default')
-      output = YAML::dump(make_configmap.target_hash)
-      expect(File.read(CONFIGMAP_CONTENT)).to eq(output)
+  context 'when --from-file is a file' do
+
+    let(:configmap_from_file) { [CONFIGMAP_DATA_FILE_PATH] }
+
+    it 'writes a ConfigMap' do
+      configmap = KubeDeployTools::ConfigMap.new(CONFIGMAP_NAME, configmap_from_file).target_hash
+      configmap_content = YAML::dump(configmap)
+
+      expect(configmap_content).to eq(CONFIGMAP_FILE_CONTENT)
+      expect(configmap['metadata']['name']).to eq(CONFIGMAP_NAME)
+      expect(configmap['metadata']['namespace']).to eq(CONFIGMAP_DEFAULT_NAMESPACE)
+      expect(configmap['metadata']['labels']).to be_nil
+      expect(configmap['data'][CONFIGMAP_DATA_FILE_NAME]).to eq(CONFIGMAP_DATA_FILE_CONTENT)
+    end
+
+    it 'writes a ConfigMap with .metadata.namespace and .metadata.labels' do
+      namespace = 'my-namespace'
+      label_name = 'app'
+      label_value = 'frontend'
+      labels = { label_name => label_value }
+
+      configmap = KubeDeployTools::ConfigMap.new(CONFIGMAP_NAME, configmap_from_file, namespace, labels).target_hash
+      configmap_content = YAML::dump(configmap)
+
+      expect(configmap['metadata']['name']).to eq(CONFIGMAP_NAME)
+      expect(configmap['metadata']['namespace']).to eq(namespace)
+      expect(configmap['metadata']['labels'][label_name]).to eq(label_value)
+      expect(configmap['data'][CONFIGMAP_DATA_FILE_NAME]).to eq(CONFIGMAP_DATA_FILE_CONTENT)
+    end
+  end
+
+  context 'when --from-file is a directory' do
+
+    let(:configmap_from_file) { [CONFIGMAP_DATA_DIRECTORY_PATH] }
+
+    it 'writes a ConfigMap' do
+      configmap = KubeDeployTools::ConfigMap.new(CONFIGMAP_NAME, configmap_from_file).target_hash
+      configmap_content = YAML::dump(configmap)
+
+      expect(configmap_content).to eq(CONFIGMAP_FILE_CONTENT)
+      expect(configmap['metadata']['name']).to eq(CONFIGMAP_NAME)
+      expect(configmap['metadata']['namespace']).to eq(CONFIGMAP_DEFAULT_NAMESPACE)
+      expect(configmap['data'][CONFIGMAP_DATA_FILE_NAME]).to eq(CONFIGMAP_DATA_FILE_CONTENT)
+    end
+  end
+
+  context 'when --from-file is key=filepath' do
+
+    let(:configmap_from_file) { ["#{CONFIGMAP_DATA_FILE_NAME}=#{CONFIGMAP_DATA_FILE_PATH}"] }
+
+    it 'writes a ConfigMap' do
+      configmap = KubeDeployTools::ConfigMap.new(CONFIGMAP_NAME, configmap_from_file).target_hash
+      configmap_content = YAML::dump(configmap)
+
+      expect(configmap_content).to eq(CONFIGMAP_FILE_CONTENT)
+      expect(configmap['metadata']['name']).to eq(CONFIGMAP_NAME)
+      expect(configmap['metadata']['namespace']).to eq(CONFIGMAP_DEFAULT_NAMESPACE)
+      expect(configmap['data'][CONFIGMAP_DATA_FILE_NAME]).to eq(CONFIGMAP_DATA_FILE_CONTENT)
     end
   end
 end
