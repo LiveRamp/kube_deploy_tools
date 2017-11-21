bb42dc63a8722ea24de75e38e029a93e43a2ac61
Julian V. Modesto <julian.modesto@liveramp.com>
2017-11-21 16:04:54 -0500

    Add shellrunner unit test

--- spec/unit/shellrunner_spec.rb
+++ spec/unit/shellrunner_spec.rb
@@ -2,24 +2,46 @@ require 'kube_deploy_tools/shellrunner'
 require 'kube_deploy_tools/formatted_logger'
 
 describe KubeDeployTools::Shellrunner do
-  let(:logger) { KubeDeployTools::FormattedLogger.build(context: CONTEXT) }
+  # Mock logger
+  let(:logger) { instance_double('logger') }
 
-  # Mock shellrunner
-  let(:status) { double(:status, success?: true) }
+  # Mock Open3 captured output
+  let(:status) { double("status", success?: true) }
+  let(:err) { nil }
   let(:stdoutput) { 'bogus command output' }
+  let(:captured_output) { [stdoutput, err, status] }
+
+  # Test setup
+  let(:shellrunner) { KubeDeployTools::Shellrunner.new(logger: logger) }
+  let(:cmd) { ['bogus', 'command'] }
 
   context "where the command fails" do
-    let(:status) { double(:status, success?: false) }
+
+    # Mock Open3 captured output
+    let(:err) { 'my err' }
+    let(:status) { double("status", success?: false) }
 
     it "raises an error with the command ran" do
-      shellrunner = KubeDeployTools::Shellrunner.new(logger: logger)
-
-      cmd = ['bogus', 'command']
-      expect(shellrunner).to receive(:run_call).with(*cmd).and_return([stdoutput, nil, status])
-
+      allow(Open3).to receive(:capture3).with(*cmd).and_return(captured_output)
       expect do
         shellrunner.check_call(*cmd)
       end.to raise_error(include(cmd.join(' ')))
+    end
+
+    it "prints the command" do
+      allow(Open3).to receive(:capture3).with(*cmd).and_return(captured_output)
+      allow(logger).to receive(:info)
+      allow(logger).to receive(:debug)
+
+      # Log the failed command and the error
+      expect(logger).to receive(:warn).with(/command failed/).once
+      expect(logger).to receive(:warn).with(err).once
+
+      shellrunner.run_call(*cmd)
+
+      # Don't log anything
+      expect(logger).to receive(:warn).at_most(0).times
+      shellrunner.run_call(*cmd, print_cmd: false)
     end
   end
 end
