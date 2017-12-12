942a93a9a232426cf64d9c7c1aa2af71b51d3854
Tiantian Tang <tiantian.tang@liveramp.com>
2017-12-11 17:40:43 -0800

    KDT should delete old build directory before each run

--- spec/unit/deploy_artifact_spec.rb
+++ spec/unit/deploy_artifact_spec.rb
@@ -6,6 +6,7 @@ LOCAL_ARTIFACT='manifests_local_staging_default'
 LOCAL_COMPRESSED_ARTIFACT="#{LOCAL_ARTIFACT}.tar.gz"
 REMOTE_ARTIFACT="http://***REMOVED***/artifactory/kubernetes-snapshot-local/FAKEPROJECT/FAKEJOBNUMBER/#{LOCAL_COMPRESSED_ARTIFACT}"
 TEST_RESOURCES='spec/resources/'
+LEFT_OVER_FILE = 'leftover_from_last_run'
 
 describe KubeDeployTools::DeployArtifact do
   let(:logger) { KubeDeployTools::FormattedLogger.build }
@@ -27,21 +28,32 @@ describe KubeDeployTools::DeployArtifact do
       local_compressed_artifact = File.join(tmp_dir, LOCAL_COMPRESSED_ARTIFACT)
       local_artifact = File.join(tmp_dir, LOCAL_ARTIFACT)
 
-      # Expect to actually `curl` and `tar -x`
-      expect(shellrunner).to receive(:run_call).with('curl', any_args)
-      expect(shellrunner).to receive(:run_call).with('tar', any_args)
+      # stub out `curl` and `tar -x`
+      allow(shellrunner).to receive(:run_call).with('curl', any_args) {
+        # Simulate artifact download with tarball copy
+        FileUtils.cp(
+          File.join(TEST_RESOURCES, LOCAL_COMPRESSED_ARTIFACT),
+          local_compressed_artifact,
+        )
 
-      # Simulate artifact download with tarball copy
-      FileUtils.cp(
-        File.join(TEST_RESOURCES, LOCAL_COMPRESSED_ARTIFACT),
-        local_compressed_artifact,
-      )
-      # Simulate uncompressing tarball with making the directory
-      FileUtils.mkdir(local_artifact)
+        [0,0,double(:success? => true)]
+      }
+
+      allow(shellrunner).to receive(:run_call).with('tar', any_args) {
+
+        # Simulate uncompressing tarball with making the directory
+        FileUtils.touch("#{local_artifact}/untared_file")
+        [0,0,double(:success? => true)]
+      }
+
+      leftover_file = "#{local_artifact}/#{LEFT_OVER_FILE}"
+      FileUtils.mkdir_p(local_artifact)
+      FileUtils.touch(leftover_file)
 
       path = deploy_artifact.path
 
       expect(Dir["#{tmp_dir}/*"]).to include(local_compressed_artifact)
+      expect(Dir["#{local_artifact}/*"]).not_to include(leftover_file)
       expect(File.directory?(local_artifact)).to be true
       expect(path).to eq(local_artifact)
     end
