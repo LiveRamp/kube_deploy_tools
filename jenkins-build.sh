a4386415c79230bb60b94adb8734f0cec2d07248
Joshua Kwan <joshua.kwan@liveramp.com>
2018-01-17 19:05:31 -0500

    hotfix: fix kdt build on docker2 slaves

--- jenkins-build.sh
+++ jenkins-build.sh
@@ -4,6 +4,16 @@ set -euo pipefail
 bundle install --with development --path=vendor/bundle
 bundle exec rake
 
+# HACK(joshk): work around docker2 slaves not having dummy .gem/credentials
+# file Can be removed when these commands are added to the VM template
+# generation stage.
+mkdir -p ~/.gem
+cat >~/.gem/credentials <<EOF
+---
+:rubygems_api_key: 'missing'
+EOF
+chmod 0600 ~/.gem/credentials
+
 case "$GIT_BRANCH" in
 */master) bundle exec rake push ;;
 esac
