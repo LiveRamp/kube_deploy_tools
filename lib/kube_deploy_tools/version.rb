30308aa41f839252dce71faf5b28cdf6b12dd111
Pratik Mallya <Pratik.Mallya@liveramp.com>
2019-01-16 17:32:58 -0800

    Bump kdt
    Build on master fails without bump

--- lib/kube_deploy_tools/version.rb
+++ lib/kube_deploy_tools/version.rb
@@ -1,6 +1,6 @@
 module KubeDeployTools
   # NOTE(jmodes): Bump patch
-  VERSION_XYZ = '2.1.10'
+  VERSION_XYZ = '2.1.11'
   def self.version_xyz
     version_xyz = VERSION_XYZ
     prerelease_notation = '.dev'
