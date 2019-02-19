c04c9b05cef59a3dd7e0e605ddb4f124f3c8b686
Julian Modesto <julian.modesto@liveramp.com>
2019-02-19 13:12:45 -0500

    Improve recursive deploy.yaml search error. (#193)
    If deploy.yaml cannot be found, then error out with a more descriptive
    error.

--- lib/kube_deploy_tools/version.rb
+++ lib/kube_deploy_tools/version.rb
@@ -1,6 +1,6 @@
 module KubeDeployTools
   # NOTE(jmodes): Bump patch
-  VERSION_XYZ = '2.1.15'
+  VERSION_XYZ = '2.1.16'
   def self.version_xyz
     version_xyz = VERSION_XYZ
     prerelease_notation = '.dev'
