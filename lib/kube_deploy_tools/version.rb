c027a0b4080b1ecde5460f39ca8fc736a1b2943a
Joshua Kwan <joshua.kwan@liveramp.com>
2019-06-26 14:27:59 -0400

    no-op to test gem-pushing

--- lib/kube_deploy_tools/version.rb
+++ lib/kube_deploy_tools/version.rb
@@ -1,6 +1,6 @@
 module KubeDeployTools
   # NOTE(jmodes): Bump patch
-  VERSION_XYZ = '2.1.24'
+  VERSION_XYZ = '2.1.25'
   def self.version_xyz
     version_xyz = VERSION_XYZ
     prerelease_notation = '.dev'
