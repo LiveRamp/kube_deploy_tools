3a6431806c25b88f5cf9abb40779550aef01bec2
Joshua Kwan <joshua.kwan@liveramp.com>
2019-10-11 23:01:14 +0000

    fix

--- lib/kube_deploy_tools/version.rb
+++ lib/kube_deploy_tools/version.rb
@@ -1,28 +1,6 @@
-require_relative 'version_auto'
+# Usually replaced by CI at the last minute on release builds.
+# The version will be equal to the tag value pushed (minus the leading 'v')
 
 module KubeDeployTools
-  def self.version_xyz
-    version_xyz = VERSION_XYZ
-    prerelease_notation = '.dev'
-    build_metadata_notation = ''
-    build_metadata_notation += ENV.has_key?('GIT_BRANCH') ? '.' + ENV.fetch('GIT_BRANCH') : ''
-    build_metadata_notation += ENV.has_key?('BUILD_ID') ? '.' + ENV.fetch('BUILD_ID') : ''
-    build_metadata_notation = build_metadata_notation.sub('origin/', '')
-    build_metadata_notation = build_metadata_notation.gsub('_', '-')
-
-    branch = ENV.fetch('GIT_BRANCH', '').sub('origin/', '')
-    if branch == 'master' || branch.start_with?('release')
-      # Jenkins master or release builds
-      return version_xyz
-    elsif ENV.has_key?('GIT_BRANCH') && ENV.has_key?('BUILD_ID')
-      # Jenkins non-master builds
-      version_xyz += prerelease_notation + build_metadata_notation
-    elsif File.exist?('/proc/1/cgroup') && File.open('/proc/1/cgroup').grep(/docker/).any?
-      # Docker builds
-      return version_xyz
-    else
-      # non-Jenkins
-      version_xyz += prerelease_notation
-    end
-  end
+  VERSION_XYZ = '9.0.0'
 end
--- lib/kube_deploy_tools/version_auto.rb
+++
@@ -1,6 +0,0 @@
-# Usually replaced by CI at the last minute on release builds.
-# The version will be equal to the tag value pushed (minus the leading 'v')
-
-module KubeDeployTools
-  VERSION_XYZ = '9.0.0'
-end
